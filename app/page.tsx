c1acfc6cdafb4bc70ebada1dc10c5c9ea4cf777e
Abdelhadi Oumar <abdelhadioumar@stud.cu.edu.eg>
2024-07-08 21:01:23 +0100

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
         <div>
             <h1 className="text-white text-3xl underline">Home</h1>
             <Button>Click me</Button>
+            <h2>Test home page...</h2>
         </div>
     )
 }
